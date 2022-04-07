8812dfcd473b5ac4f5f7a22ef9e8e819b6f2e652
shimo8810 <shimo.8810@gmail.com>
2022-04-07 17:56:35 +0900

    [add] usecase層の実装

---
+++ todos/src/usecase/todos.rs
@@ -0,0 +1,26 @@
+use crate::domain::{Task, TaskFilter, TaskId, TaskRepository};
+pub struct UseCase<'r, Repo: TaskRepository> {
+    repository: &'r Repo,
+}
+
+impl<'r, Repo: TaskRepository> UseCase<'r, Repo> {
+    pub fn new(repository: &'r Repo) -> Self {
+        Self { repository }
+    }
+
+    pub fn get_task_list(&self, filter: &TaskFilter) -> Result<Vec<Task>, String> {
+        self.repository.list(filter)
+    }
+
+    pub fn add_task(&self, task: &Task) -> Result<(), String> {
+        self.repository.insert(task)
+    }
+
+    pub fn delete(&self, id: &TaskId) -> Result<(), String> {
+        self.repository.delete(id)
+    }
+
+    pub fn update_task(&self, task: &Task) -> Result<(), String> {
+        self.repository.update(task)
+    }
+}
