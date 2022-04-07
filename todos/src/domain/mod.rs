a57dc01ec5f53c69405e54205841d54d50071a6a
shimo8810 <shimo.8810@gmail.com>
2022-04-07 17:25:06 +0900

    [update] repositoryのパスを修正

--- todos/src/domain/mod.rs
+++ todos/src/domain/mod.rs
@@ -4,6 +4,7 @@ pub mod task_body;
 pub mod task_id;
 pub mod task_state;
 
+pub use self::repository::TaskRepository;
 pub use self::task::Task;
 pub use self::task_body::TaskBody;
 pub use self::task_id::TaskId;
