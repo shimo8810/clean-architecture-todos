c9ccb072593c69cdc264b15434cb56dfa1fb1a4f
shimo8810 <shimo.8810@gmail.com>
2022-04-07 19:51:39 +0900

    [update] 文字列へ/からの変換を実装

--- todos/src/domain/task_state.rs
+++ todos/src/domain/task_state.rs
@@ -1,5 +1,33 @@
+use std::fmt;
+use std::str::FromStr;
+
 #[derive(Debug, PartialEq)]
 pub enum TaskState {
     Active,
     Completed,
+}
+
+impl fmt::Display for TaskState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // let str = match self {
+
+        // }
+        let str = match *self {
+            Self::Active => "Active",
+            Self::Completed => "Completed",
+        };
+
+        write!(f, "{}", str)
+    }
+}
+
+impl FromStr for TaskState {
+    type Err = String;
+    fn from_str(x: &str) -> Result<Self, Self::Err> {
+        match x {
+            "Active" => Ok(Self::Active),
+            "Completed" => Ok(Self::Active),
+            _ => Err("incorrect string value".to_string()),
+        }
+    }
 }
