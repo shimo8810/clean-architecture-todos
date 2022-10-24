8045c774e4fac58dac4bd4bf9c083bba22695dbb
shimo <shimo.8810@gmail.com>
2022-10-24 20:22:03 +0900

    [update] 必要なモジュールを整理

--- todos/src/domain/mod.rs
+++ todos/src/domain/mod.rs
@@ -1,13 +1,6 @@
+pub mod error;
 pub mod repository;
 pub mod task;
 pub mod task_body;
-pub mod task_filter;
 pub mod task_id;
 pub mod task_state;
-
-pub use self::repository::TaskRepository;
-pub use self::task::Task;
-pub use self::task_body::TaskBody;
-pub use self::task_filter::TaskFilter;
-pub use self::task_id::TaskId;
-pub use self::task_state::TaskState;
