3a2297e0c49ce85090e92edab20e4ea7a84dfb04
shimo <shimo.8810@gmail.com>
2022-10-29 22:36:29 +0900

    [update] クレートのパスを省略できるように変更

--- todos/src/domain/mod.rs
+++ todos/src/domain/mod.rs
@@ -4,3 +4,9 @@ pub mod task;
 pub mod task_body;
 pub mod task_id;
 pub mod task_state;
+
+pub use repository::TaskRepository;
+pub use task::Task;
+pub use task_body::TaskBody;
+pub use task_id::TaskId;
+pub use task_state::TaskState;
