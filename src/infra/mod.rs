ceee5fa41eb339a7a5edf71a2ef7dcc1c6f8ed3d
shimo <shimo.8810@gmail.com>
2022-10-29 23:33:16 +0900

    [update] コネクションを確立する関数の実装を追加

--- src/infra/mod.rs
+++ src/infra/mod.rs
@@ -2,3 +2,20 @@ pub mod error;
 pub mod model;
 pub mod repository;
 pub mod schema;
+
+pub use repository::DbPool;
+pub use repository::PgTaskRepository;
+
+use diesel::{
+    r2d2::{self, ConnectionManager},
+    PgConnection,
+};
+
+pub fn establish_connection(database_url: &str) -> PgTaskRepository {
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create DB connection pool.");
+
+    PgTaskRepository::new(pool)
+}
