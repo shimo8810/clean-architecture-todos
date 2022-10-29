e9d470c095fcf895a20302348dcc095affe11959
shimo <shimo.8810@gmail.com>
2022-10-29 23:32:58 +0900

    [update] 最小表現に修正

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,1 @@
-// use anyhow::Result;
-// use todos::infra::db::mem_repository::MemoTaskReposiory;
-// use todos::infra::server::run_server;
-
-// #[tokio::main]
-// async fn main() -> Result<()> {
-//     let repo = MemoTaskReposiory::new();
-//     run_server(repo).await?;
-//     Ok(())
-// }
-
-fn main() {
-    //
-}
+fn main() {}
