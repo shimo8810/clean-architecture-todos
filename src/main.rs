972e4fc64e63aab81222364e3ec4dc36328f4478
shimo <shimo.8810@gmail.com>
2022-10-30 19:24:14 +0900

    [update] todo-apiが動作するように実装を追加

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use todos::infra::establish_connection;
+use todos::server::run;
+use todos::usecase::task::UseCase;
+
+fn main() -> std::io::Result<()> {
+    let repo = establish_connection();
+    let usecase = UseCase::new(repo);
+    run(usecase)
+}
